f1fd103ebe4aed362f13313dc5296dd47a1d01ee
Jay McConnell <jmmccon@amazon.com>
2018-12-07 15:01:29 -0800

    ignore errors with cwl service

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,7 +3,7 @@
 source ${P}
 export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 qs_cloudwatch_install
-systemctl stop awslogs
+systemctl stop awslogs || true
 cat << EOF > /var/awslogs/etc/awslogs.conf
 [general]
 state_file = /var/awslogs/state/agent-state
@@ -17,7 +17,7 @@ initial_position = start_of_file
 datetime_format = %b %d %H:%M:%S
 
 EOF
-systemctl start awslogs
+systemctl start awslogs || true
 
 if [ -f /quickstart/pre-install.sh ]
 then
