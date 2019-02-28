c1a36de4bf56d5694252fcaa5be1012da92180f0
Jay McConnell <jmmccon@amazon.com>
2019-02-28 11:48:44 -0800

    needed for pcsc-lite-devel - java dependency

--- scripts/redhat_ose-register-3.11.sh
+++ scripts/redhat_ose-register-3.11.sh
@@ -20,7 +20,8 @@ qs_retry_command 20 subscription-manager repos --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-ose-3.11-rpms" \
     --enable="rhel-7-fast-datapath-rpms" \
     --enable="rhel-7-server-ansible-2.6-rpms" \
-    --enable="rh-gluster-3-client-for-rhel-7-server-rpms"
+    --enable="rh-gluster-3-client-for-rhel-7-server-rpms" \
+    --enable="rhel-7-server-optional-rpms"
 
 var=($(subscription-manager identity))
 UUID="${var[2]}"
